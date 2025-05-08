0f5fddd43a9fc7c2b4a3d1032e512b390b58d89d
Manoj-kumar423 <64767579+Manoj-kumar423@users.noreply.github.com>
2025-05-09 00:16:06 +0530

    Update app.py
    update flask name to Flask

--- apps/app.py
+++ apps/app.py
@@ -1,8 +1,8 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return{"message:" "hello from flask!"}
+  return{"message": "hello from flask!"}
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
